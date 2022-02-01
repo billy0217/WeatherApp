a339fdf433f73e56fb9bdcbdcfcf57c719302c81
billyh0217 <billy.han@terabyte.co.nz>
2022-02-01 14:44:36 +1300

    add font icon, add weather forecast API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,15 +4,12 @@
       <input 
         class="c-search-input"
         type="text"
-        placeholder="Search..." 
+        placeholder="Location Search..." 
         v-model="query"
         @keypress="fetchWeather"
       />
     </div>
-
-     {{lat}}
-        <p v-if="long"> {{long}}</p>
-
+  
     <div class="c-weather-wrap" v-if="typeof weather.main != 'undefined'">
       <div class="c-weather__location-box">
         <div class="c-weather__location">{{weather.name}}, {{weather.sys.country}}</div>
@@ -21,16 +18,21 @@
 
       <div class="c-weather__box">
         <div class="c-weather__temp">{{ Math.round(weather.main.temp) }}°c</div>
-        <div class="c-weather__state">{{ weather.weather[0].main }}</div>
+        <div class="c-weather__state-icon">
+          
+        </div>
+        <div class="c-weather__discription">{{ weather.weather[0].description }}</div>
+        <div class="c-weather__state">L: {{ Math.round(weather.main.temp_min) }}°c H: {{ Math.round(weather.main.temp_max) }}°c</div>
       </div>
 
-      <div class="c-weather__list">
-        <div class="c-weater_list-item"></div>
-        <div class="c-weater_list-item"></div>
-        <div class="c-weater_list-item"></div>
-        <div class="c-weater_list-item"></div>
-        <div class="c-weater_list-item"></div>
-        <div class="c-weater_list-item"></div>
+      <div class="c-weather__list-wrap" v-if="typeof forecast.daily != 'undefined'">
+        <div v-for="(daily, index) in forecast.daily" :key="index" class="c-weater_list-item">
+          <p class="c-weather__list-date" v-if="index === 0">Today</p>
+          <p class="c-weather__list-date" v-else>{{ getDate(daily.dt) }}</p>
+          <div class="c-weather__list-icon-wrapper" ><i class="c-weather__list-icon" :class="'iconf-'+ daily.weather[0].icon"></i></div>
+          <p class="c-weather__list-discription">{{daily.weather[0].description}}</p>
+          <p class="c-weather__list-state">L:{{ Math.round(daily.temp.min) }}°c  H: {{ Math.round(daily.temp.max) }}°c</p>
+        </div>
       </div>
 
     </div>
@@ -40,9 +42,6 @@
 
 <script>
 import { readonly } from 'vue';
-// @ is an alias to /src
-//import HelloWorld from '@/components/HelloWorld.vue'
-
 import {ApiKeys} from '@/config/config'
 
 export default {
@@ -53,7 +52,7 @@ export default {
       url_base: 'https://api.openweathermap.org/data/2.5/',
       query: '',
       weather: {},
-      weatherInfo: {},
+      forecast: {},
       location: "",
       lat: null,
       long: null
@@ -76,14 +75,6 @@ export default {
       
       _this.$data.lat = position.coords.latitude;
       _this.$data.long = position.coords.longitude;
-
-
-     // console.log(position.coords.latitude);
-     // console.log(position.coords.longitude);
-     // console.log(position.coords);
-      //console.log("show positon")
-      //return position.coords.latitude;
-      //console.log(lat);
     }
 
     function error(err) {
@@ -95,7 +86,7 @@ export default {
   },
   methods: {
 
-    fetchWeatherInfo(){
+    fetchForecast(){
       fetch(`${this.url_base}/onecall?lat=${this.lat}&lon=${this.long}&exclude=minutely,hourly,alerts&units=metric&APPID=${this.api_key}`)
         .then(res => {
           return res.json();
@@ -105,7 +96,7 @@ export default {
 
     setInfo(res) {
       console.log(res);
-      this.weatherInfo = res;
+      this.forecast = res;
     },
 
     // fetch weather 
@@ -116,14 +107,13 @@ export default {
           return res.json();
         })
         .then(this.setResult)
+        .then(this.fetchForecast)
       }
     },
 
-
     // update weather data 
     setResult(result) {
-      console.log(result)
-      console.log(this.long)
+      console.log(result);
       this.weather = result;
       this.lat = result.coord.lat;
       this.long = result.coord.lon;
@@ -151,21 +141,29 @@ export default {
       return `${day} ${date} / ${month} / ${year} `;
     },
 
+    // convert timestamps to human date time
+    getDate(timestamps){
+      const date = new Date(timestamps * 1000);
+      const options = { weekday: 'short' };
+      return date.toLocaleDateString('en-US',options);
+    },
+
     // convert lower cases
     lowerCase(str){
       return `${str.toLowerCase()}`;
     }
   },
-  updated(){
+  beforeUpdate(){
     // 
-    if(Object.keys(this.weatherInfo).length === 0){
-      this.fetchWeatherInfo()
+    if(Object.keys(this.forecast).length === 0){
+      //this.fetchForecast()
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
+
   $black: #000;
   $white: #fff;
 
@@ -202,7 +200,7 @@ export default {
     flex-direction: column;
     justify-content: center;
     align-items: stretch;
-    flex: 1;
+    margin-top: 30px;
   }
 
   .c-weather__location-box {
@@ -243,10 +241,91 @@ export default {
 
   .c-weather__state {
     color: $white;
-    font-size: 30px;
+    font-size: 25px;
     font-weight: bold;
-    text-shadow: 3px 6px rgba($black, 0.25);
+  }
 
+  .c-weather__discription {
+    color: $white;
+    font-size: 25px;
+    font-weight: bold;
+  }
+
+  .c-weather__list-wrap {
+    display: grid;
+    grid-gap: 10px;
+    grid-template-columns: repeat(1, 1fr);
+    margin-top: 50px;
+    @media (min-width:1024px){
+      grid-template-columns: repeat(8, 1fr);
+      margin-top: 80px;
+    }
+  }
+
+  .c-weater_list-item{
+    display: grid;
+    justify-content: space-between;
+    grid-template-columns: repeat(3, 1fr);
+    align-items: center;
+    @media (min-width:680px){
+      grid-template-columns: repeat(5, 1fr);
+    }
+    
+    @media (min-width:1024px){
+      grid-template-columns: repeat(1, 1fr);
+      align-items: flex-start;
+    }
+  }
+
+  .c-weather__list-date {
+    color: $white;
+    font-size: 18px;
+    font-weight: bold;
+    margin-bottom: 5px;
+
+    @media (min-width:680px){
+      grid-column:  auto;
+    }
+
+    @media (min-width:1024px){
+      margin-bottom: 15px;
+    }
+  }
+  
+  .c-weather__list-icon-wrapper{
+    @media (min-width:1024px){
+      margin-bottom: 15px;
+    }
+  }
+
+  .c-weather__list-state-icon {
+    width: 30px;
+    @media (min-width:1024px){
+      width: 50px;
+    }
+  }
+
+  .c-weather__list-icon {
+    font-size: 40px;
+    color: $white;
+  }
+
+  .c-weather__list-discription {
+    display: none;
+    @media (min-width:680px){
+      display: block;
+      grid-column:  span 2;
+    }
+    @media (min-width:1024px){
+      grid-column:  auto;
+    }
+  }
+
+  .c-weather__list-discription,
+  .c-weather__list-state {
+    color: $white;
+    font-size: 16px;
+    margin-bottom: 5px;
   }
 
 </style>
